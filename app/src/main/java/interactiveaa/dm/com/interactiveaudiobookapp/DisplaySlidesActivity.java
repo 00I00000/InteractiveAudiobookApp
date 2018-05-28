650332928f26b3941aa887d489a6bb40007b1851
Frank Ling <f.ling336@gmail.com>
2018-05-28 06:51:50 +0200

    removed integrated VC, custom button design

--- app/src/main/java/interactiveaa/dm/com/interactiveaudiobookapp/DisplaySlidesActivity.java
+++ app/src/main/java/interactiveaa/dm/com/interactiveaudiobookapp/DisplaySlidesActivity.java
@@ -1,9 +1,16 @@
 package interactiveaa.dm.com.interactiveaudiobookapp;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Toast;
+
+import java.io.FileOutputStream;
 
 public class DisplaySlidesActivity extends AppCompatActivity {
 
@@ -24,6 +31,21 @@ public class DisplaySlidesActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.slides);
         hideNavBar();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1000);
+        }
+    }
+
+    public void saveFile(String file, String text) {
+        try {
+            FileOutputStream fos = openFileOutput(file, Context.MODE_PRIVATE);
+            fos.write(text.getBytes());
+            fos.close();
+            Toast.makeText(this, "Saved!", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Error saving file!", Toast.LENGTH_SHORT).show();
+        }
     }
 
     //SlidesFragment slidesFragment = (SlidesFragment) getSupportFragmentManager().findFragmentById(R.id.slide);
