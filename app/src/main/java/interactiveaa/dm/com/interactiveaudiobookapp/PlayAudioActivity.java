d14a054578d99ed793edf68dab03cd3396b8a3b5
Frank Ling <f.ling336@gmail.com>
2018-06-15 14:25:56 +0200

    Minor bug fix, layout improved

--- app/src/main/java/interactiveaa/dm/com/interactiveaudiobookapp/PlayAudioActivity.java
+++ app/src/main/java/interactiveaa/dm/com/interactiveaudiobookapp/PlayAudioActivity.java
@@ -195,7 +195,7 @@ public class PlayAudioActivity extends AppCompatActivity {
                             | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                             | View.SYSTEM_UI_FLAG_FULLSCREEN
                             | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-        } else {
+        } else if (mediaPlayer != null){
             mediaPlayer.start();
         }
     }
